0f23346934f938a3f07e441f39c33b967f119a33
Eli Claros <eclaros2@email.cpcc.edu>
2023-09-26 11:26:20 -0400

    Added file and able to read input
    Added file that contains policy information and Modified to read input from a file

--- Project_Eli_Claros.java
+++ Project_Eli_Claros.java
@@ -1,65 +1,74 @@
 //import the Scanner
 import java.util.Scanner;
+import java.io.*;
 
 public class Project_Eli_Claros
 {
    public static void main(String[] args)
    {
-      //make a Scanner object for input
-      Scanner keyboard = new Scanner(System.in);   
       
-      //all the variables
-      int policyNumber;
-      String providerName;
-      String policyHolderFirstName;
-      String policyHolderLastName;
-      int policyHolderAge;
-      String policyHolderSmokingStatus;
-      double policyHolderHeight;
-      double policyHolderWeight;
+      try
+      {
+         File file = new File("PolicyInformation.txt");
+         
+         Scanner inputFile = new Scanner(file);   
+         
+         
+         int policyNumber, policyHolderAge;
+         
+         String providerName, policyHolderFirstName, policyHolderLastName, policyHolderSmokingStatus;
+         
+         double policyHolderHeight, policyHolderWeight;
+         
+         
+         ArrayList<Policy> policies = new ArrayList<>();
+         
+         
+         while(inputFile.hasNext())       
+         {
+         
+            policyNumber = inputFile.nextInt();  
+            providerName = inputFile.nextLine();
+            policyHolderFirstName = inputFile.nextLine();
+            policyHolderLastName = inputFile.nextLine();
+            policyHolderAge = inputFile.nextInt();
+            policyHolderSmokingStatus = inputFile.nextLine();
+            policyHolderHeight = inputFile.nextDouble();
+            policyHolderWeight = inpuFile.nextDouble();
+            
+         
+            if(inputFile.hasNext())
+            { 
+               inputFile.nextLine();
+               inputFile.nextLine();
+            }
+         
+            Policy policy = new Policy(policyNumber, providerName, policyHolderFirstName, policyHolderLastName, policyHolderAge, policyHolderSmokingStatus, policyHolderHeight, policyHolderWeight);
+         
+            policies.add(policy); 
+         
+         }
       
-      //all the imputs
-      System.out.print("Please enter the Policy Number: ");
-      policyNumber = keyboard.nextInt();
+         inputFile.close();
+         //creating the policy object with arguments
+                  
+         //all the outputs and calling the getter methods
+         System.out.println("\n\nPolicy Number: " + policy.getPolicyNumber());
+         System.out.println("\nProvider Name: " + policy.getProviderName());
+         System.out.println("\nPolicyholder's First Name: " + policy.getPolicyHolderFirstName());
+         System.out.println("\nPolicyholder's Last Name: " + policy.getPolicyHolderLastName());
+         System.out.println("\nPolicyholder's Age: " + policy.getPolicyHolderAge());
+         System.out.println("\nPolicyholder's Smoking Status: " + policy.getPolicyHolderSmokingStatus());
+         System.out.printf("\nPolicyholder's Height: %.2f", policy.getPolicyHolderHeight());
+         System.out.printf("\n\nPolicyHolder's Weight: %,.2f", policy.getPolicyHolderWeight());
+         System.out.printf("\n\nPolicyholder's BMI: %.2f", policy.getBMI());
+         System.out.printf("\n\nPolicy Price: $%,.2f", policy.calculatePolicyPrice());
+         
+      }   
       
-      keyboard.nextLine();
-      
-      System.out.print("\nPlease enter the Provider Name: ");
-      providerName = keyboard.nextLine();
-      
-      System.out.print("\nPlease enter the Policyholder's First Name: ");
-      policyHolderFirstName = keyboard.nextLine();
-      
-      System.out.print("\nPlease enter the Policyholder's Last Name: ");
-      policyHolderLastName = keyboard.nextLine();
-      
-      System.out.print("\nPlease enter the Policyholder's Age: ");
-      policyHolderAge = keyboard.nextInt();
-      
-      keyboard.nextLine();
-      
-      System.out.print("\nPlease enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-      policyHolderSmokingStatus = keyboard.nextLine();
-      
-      System.out.print("\nPlease enter the Policyholder's Height (in inches): ");
-      policyHolderHeight = keyboard.nextDouble();
-      
-      System.out.print("\nPlease enter the Policyholder's Weight (in pounds): ");
-      policyHolderWeight = keyboard.nextDouble();
-      
-      //creating the policy object with arguments
-      Policy policy = new Policy(policyNumber, providerName, policyHolderFirstName, policyHolderLastName, policyHolderAge, policyHolderSmokingStatus, policyHolderHeight, policyHolderWeight);
-      
-      //all the outputs and calling the getter methods
-      System.out.println("\n\nPolicy Number: " + policy.getPolicyNumber());
-      System.out.println("\nProvider Name: " + policy.getProviderName());
-      System.out.println("\nPolicyholder's First Name: " + policy.getPolicyHolderFirstName());
-      System.out.println("\nPolicyholder's Last Name: " + policy.getPolicyHolderLastName());
-      System.out.println("\nPolicyholder's Age: " + policy.getPolicyHolderAge());
-      System.out.println("\nPolicyholder's Smoking Status: " + policy.getPolicyHolderSmokingStatus());
-      System.out.printf("\nPolicyholder's Height: %.2f", policy.getPolicyHolderHeight());
-      System.out.printf("\n\nPolicyHolder's Weight: %,.2f", policy.getPolicyHolderWeight());
-      System.out.printf("\n\nPolicyholder's BMI: %.2f", policy.getBMI());
-      System.out.printf("\n\nPolicy Price: $%,.2f", policy.calculatePolicyPrice());
+      catch(IOException ex)
+      {
+         System.out.println("Something went wrong reading the file: " + ex.getMessage());
+      }
    }
 }
