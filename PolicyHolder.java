973c81be8c4ef31517220ee83091bd00bd635e22
Eli Claros <eclaros2@email.cpcc.edu>
2023-10-23 16:39:25 -0400

    Moved fields/methods to PolicyHolder
    Moved fields and methods to the PolicyHolder class

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -1,4 +1,115 @@
 public class PolicyHolder
 {
+   //attributes for the class (fields)
+   private String policyHolderFirstName;
+   private String policyHolderLastName;
+   private int policyHolderAge;
+   private String policyHolderSmokingStatus;
+   private double policyHolderHeight;
+   private double policyHolderWeight;
    
+   /**
+    No-argument constructor for the Policy class
+    Initializes all variables to their default values
+    */
+   public PolicyHolder() 
+   {
+      //initialize the variables
+      this.policyHolderFirstName = "";
+      this.policyHolderLastName = "";
+      this.policyHolderAge = 0;
+      this.policyHolderSmokingStatus = "";
+      this.policyHolderHeight = 0;
+      this.policyHolderWeight = 0;
+   }
+   
+   /**
+    Constructor for the Policy class that accepts the attributes
+    
+    @param policyHolderFirstName The first name of the policy holder
+    @param policyHolderLastName The last name of the policy holder
+    @param policyHolderAge The age of the policy holder
+    @param policyHolderSmokingStatus The smoking status of the policy holder
+    @param policyHolderHeight The height of the policy holder in inches
+    @param policyHolderWeight The weight of the policy holder in pounds
+    */
+   public PolicyHolder(String policyHolderFirstName, String policyHolderLastName, int policyHolderAge, String policyHolderSmokingStatus, double policyHolderHeight, double policyHolderWeight)
+   {
+      this.policyHolderFirstName = policyHolderFirstName;
+      this.policyHolderLastName = policyHolderLastName;
+      this.policyHolderAge = policyHolderAge;
+      this.policyHolderSmokingStatus = policyHolderSmokingStatus;
+      this.policyHolderHeight = policyHolderHeight;
+      this.policyHolderWeight = policyHolderWeight;
+   }   
+   
+   /**
+    Get the first name of the policy holder
+    
+    @return The first name of the policy holder
+    */
+   public String getPolicyHolderFirstName()
+   {
+      return policyHolderFirstName;
+   }
+   
+   /**
+    Get the last name of the policy holder
+    
+    @return The last name of the policy holder
+    */
+   public String getPolicyHolderLastName()
+   {
+      return policyHolderLastName;
+   }
+   
+   /**
+    Get the age of the policy holder
+    
+    @return The age of the policy holder
+    */
+   public int getPolicyHolderAge()
+   {
+      return policyHolderAge;
+   }
+   
+   /**
+    Get the smoking status of the policy holder
+    
+    @return The smoking status ("smoker" or "non-smoker")
+    */
+   public String getPolicyHolderSmokingStatus()
+   {
+      return policyHolderSmokingStatus;
+   }
+   
+   /**
+    Get the height of the policy holder in inches
+    
+    @return The height in inches
+    */
+   public double getPolicyHolderHeight()
+   {
+      return policyHolderHeight;
+   }
+   
+   /**
+    Get the weight of the policy holder in pounds
+    
+    @return The weight in pounds
+    */
+   public double getPolicyHolderWeight()
+   {
+      return policyHolderWeight;
+   }
+   
+   /**
+    Calculate the BMI of the policy holder
+    
+    @return The BMI value.
+    */
+   public double getBMI()
+   {
+      return (policyHolderWeight * 703 ) / (policyHolderHeight * policyHolderHeight);
+   }
 }
